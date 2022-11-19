4f9914c247e4dde11abeee562d788459d3cb1728
zhang3637 <1435993637@qq.com>
2022-11-19 17:41:19 +0800

    新增了文件

---
+++ linux命令/linux常用命令.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @author ZhangSir
+# @date 2022/11/19
+# @file linux常用命令.py
---
+++ linux命令/windows常用命令.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @author ZhangSir
+# @date 2022/11/19
+# @file windows常用命令.py
